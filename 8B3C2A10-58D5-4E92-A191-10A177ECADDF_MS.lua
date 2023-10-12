ba51647053f97eef6819f8127418fed80c047eed
awdwadwa213eawdaw <142877519+awdwadwa213eawdaw@users.noreply.github.com>
2023-10-12 19:39:47 +0200

    Created 8B3C2A10-58D5-4E92-A191-10A177ECADDF_MS.lua

---
+++ 8B3C2A10-58D5-4E92-A191-10A177ECADDF_MS.lua
@@ -0,0 +1,26 @@
+return [[
+Tuesday, June 28, 2016
+	Added animations to in-battle characters:
+		Idle
+		Throw Pokeball
+		Walk
+	Fixed logic in the move Fly; previously skipped charging turn
+	+1 (see internal log)]]..
+--	added fake alt formes: Dark Palkia, Red Umbreon, Blue Victini, Dark Ice Ho-Oh
+	[[
+
+Saturday, June 25, 2016
+	Opened Cragonos Mines / Cragonos Cliffs
+
+Friday, June 24, 2016 [Colosseum Only]
+	Added background music to Battle Colosseum
+
+Saturday, June 18, 2016
+	Bug fixes:
+		Canceling evolution led to black screen
+		Net Ball wouldn't respond when attempted to use
+		Music loops were ending randomly
+		+3 (see internal log)]]
+--		os.time() based on pc internal clock
+--		"Go! Arceus-fire"
+--		Illegal Pokemon travel to Trade Resort if in PC
